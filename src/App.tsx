1603486e724564c333a88c2a8b74ade4c99b79f1
Ali Najdi <ali.najdi225@outlook.com>
2025-04-08 12:33:23 +0300

    feat: deploy project

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import Image1 from './assets/Image1.png'
+import Image2 from './assets//Image2.png'
+import Image3 from './assets/Image3.png'
+import Image4 from './assets/Image4.png'
+import Image5 from './assets/Image5.png'
+import Image6 from './assets/Image6.png'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+
+     <div className='container'>
+    <img src={Image1}/>
+    <img src={Image2}/>
+    <img src={Image3}/>
+    <img src={Image4}/>
+    <img src={Image5}/>
+    <img src={Image6}/>
+     </div>
+
+  )
+}
+
+export default App
